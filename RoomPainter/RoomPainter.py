a4170d4932906e67c876392d7eb74a798339923a
Alex Brew <abrew840@gmail.com>
2021-12-02 09:49:59 +0000

    edits

--- RoomPainter/RoomPainter.py
+++ RoomPainter/RoomPainter.py
@@ -2,13 +2,13 @@ def totalAreaOfWall():
     total = 0
     numWalls = int(input('How many walls do you have? '))
     for i in range(numWalls):
-        base = int(input(f'Enter the base of wall {i+1}: '))
+        base = int(input(f'\nEnter the base of wall {i+1}: '))
         height = int(input(f'Enter the height of wall {i+1}: '))
         areaWall = base*height
         total += areaWall
     return total
 def costOfPaint():
-    litres = int(input('Enter the litres of paint you have: '))
+    litres = int(input('\nEnter the litres of paint you have: '))
     cost = int(input('What is the price of paint per litre? '))
     costPaint = litres*cost
     return costPaint
