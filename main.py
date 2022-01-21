56482973a1cd3c35ea58e0a023f282437def84a5
Alexandros Brew <44899454+AlexandrosBrew@users.noreply.github.com>
2022-01-21 09:42:36 +0000

    clear console main.py

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import os
-
+import time
 mainDirectory = os.getcwd()
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 while True:
-    dirlist = os.listdir()
-    removedFiles = [".git", ".upm", "poetry.lock",
-                    "pyproject.toml", "README.md", "main.py", ".DS_Store", ".replit"]
-    for a in removedFiles:
-        try:
-            dirlist.remove(a)
-        except:
-            pass
-    for i in range(len(dirlist)):
-        print("%s: %s" % (i+1, dirlist[i]))
-    print("-1: Back to start")
+	dirlist = os.listdir()
+	removedFiles = [".git", ".upm", "poetry.lock",
+									"pyproject.toml", "README.md", "main.py", ".DS_Store", ".replit"]
+	for a in removedFiles:
+			try:
+					dirlist.remove(a)
+			except:
+					pass
+	for i in range(len(dirlist)):
+			print("%s: %s" % (i+1, dirlist[i]))
+	print("-1: Back to start")
 
-    userInput = int(input("Selection: "))
-    if userInput == -1:
-        os.chdir(mainDirectory)
-    elif dirlist[userInput-1].endswith(".py"):
-        os.system('python "%s"' % dirlist[userInput-1])
-        print("="*50)
-        print()
-    else:
-        os.chdir(dirlist[userInput-1])
-    print()
+	userInput = int(input("Selection: "))
+	clearConsole()
+	try:
+		if userInput == -1:
+			os.chdir(mainDirectory)
+		elif dirlist[userInput-1].endswith(".py"):
+				os.system('python "%s"' % dirlist[userInput-1])
+				print("="*50)
+				print()
+		else:
+				os.chdir(dirlist[userInput-1])
+	except:
+		print("Invalid selection.\nTry again.")
+		time.sleep(1.5)
+		clearConsole()
+	print()
