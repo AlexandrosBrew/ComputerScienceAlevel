cbc861e9f18622e51d4912dbc68b6e47e75c365b
Alex Brew <abrew840@gmail.com>
2022-02-02 12:20:50 +0000

    yes

---
+++ StacksandQueues/reverseString.py
@@ -0,0 +1,14 @@
+import string
+import stack
+
+stringStack = stack.Stack(10)
+
+inp = str(input("Enter your string:"))
+
+for i in range(len(inp)):
+    stringStack.push(inp[i])
+    stringStack.printStack()
+
+for i in range(len(inp)):
+    stringStack.pop()
+    stringStack.printStack()
